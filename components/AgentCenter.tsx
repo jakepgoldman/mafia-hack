f9975d4f982aee7fcbce29676d8805f22dad9109
avasisht23 <a.vasisht98@gmail.com>
2024-11-09 21:51:53 -0500

    feat: fix agent center

--- components/AgentCenter.tsx
+++ components/AgentCenter.tsx
@@ -1,35 +1,39 @@
 "use client";
 
 import { Avatar, Card, Grid, Text } from "@mantine/core";
+import Player from "../app/game/player";
 import { Game } from "../app/page";
 
 export function AgentCenter({ game }: { game: Game }) {
-
-    const handleClick = (player) => {
-        player.speak();
-
-    }
-    return (
-        <Grid gutter="md">
-            {game.getPlayers().map((player, index) => (
-                <Grid.Col key={index} span={4}>
-
-                    <Card onClick={() => handleClick(player)} shadow="sm" padding="lg" radius="md" withBorder>
-                        <Avatar
-                            src={player.getState().avatarUrl}
-                            size="xl"
-                            styles={{
-                                image: {
-                                    objectFit: 'cover',       // Ensures the image fills the container
-                                    objectPosition: 'center', // Adjusts the cropping focus
-                                },
-                            }}
-                        />
-                        <Text>{player.getState().name}</Text>
-                        <Text>{player.getState().isAlive ? "alive" : "dead"}</Text>
-                    </Card>
-                </Grid.Col>
-            ))}
-        </Grid>
-    );
+  const handleClick = (player: Player) => {
+    player.speak();
+  };
+  return (
+    <Grid gutter="md">
+      {game.players.map((player, index) => (
+        <Grid.Col key={index} span={4}>
+          <Card
+            onClick={() => handleClick(player)}
+            shadow="sm"
+            padding="lg"
+            radius="md"
+            withBorder
+          >
+            <Avatar
+              src={player.getState().avatarUrl}
+              size="xl"
+              styles={{
+                image: {
+                  objectFit: "cover", // Ensures the image fills the container
+                  objectPosition: "center", // Adjusts the cropping focus
+                },
+              }}
+            />
+            <Text>{player.getState().name}</Text>
+            <Text>{player.getState().isAlive ? "alive" : "dead"}</Text>
+          </Card>
+        </Grid.Col>
+      ))}
+    </Grid>
+  );
 }
